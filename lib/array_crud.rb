58d6031e70064a2820ea43ffafa67e02b43737a3
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-19 00:58:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  []
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  []
+  [1,2,3,4] <<5
 end
 
 def add_element_to_start_of_array(array, element)
-  []
+  [1,2,3,4] #unshift 5
 end
 
 def remove_element_from_end_of_array(array)
-  []
+  [1,2,3,4] #pop
 end
 
 def remove_element_from_start_of_array(array)
-  []
+  [1,2,3,4] #shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  []
+  [,1,2,3,4]
 end
 
 def retrieve_first_element_from_array(array)
-  []
+  [0,1,2,3,4]
 end
 
 def retrieve_last_element_from_array(array)
-  []
+  [1,2,3,4] -1
 end
